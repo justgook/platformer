07a64e19b0dc8be483c75a44b116741d213315d1
Roman Potashow <justgook@gmail.com>
2019-08-07 14:08:36 +0300

    Start of noise

---
+++ lib/elm-noise/src/Noise.elm
@@ -0,0 +1,16 @@
+module Noise exposing (..)
+
+{-
+   https://github.com/Auburns/FastNoise
+   https://www.redblobgames.com/articles/noise/introduction.html
+       * Value Noise 2D, 3D
+       * Perlin Noise 2D, 3D
+       * Simplex Noise 2D, 3D, 4D
+       * Cubic Noise 2D, 3D
+       * Gradient Perturb 2D, 3D
+       * Multiple fractal options for all of the above
+       * Cellular (Worley) Noise 2D, 3D
+       * Continuous Cave Noise 3D
+       * White Noise 2D, 3D, 4D
+       * Supports floats or doubles
+-}
